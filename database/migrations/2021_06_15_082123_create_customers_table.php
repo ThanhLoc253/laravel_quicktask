3fe31da07b018f75cfe41195ef63d453c26f89df
ADMIN <thanhloc@ubuntu.ubuntu-domain>
2021-06-15 15:30:09 +0700

    init migration

--- database/migrations/2021_06_15_082123_create_customers_table.php
+++ database/migrations/2021_06_15_082123_create_customers_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
      *
-     *@return void
+     * @return void
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('age');
+            $table->string('address');
+            $table->string('email');
+            $table->integer('phone');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 }
